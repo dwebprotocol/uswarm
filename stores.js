6f7b38fbaaa3bd90b84db44a8ad561ae3ad0ba25
shikhars371 <shikhars371@gmail.com>
2021-05-24 10:31:03 +0530

    .

--- stores.js
+++ stores.js
@@ -7,10 +7,10 @@ const {
 const { PassThrough } = require('stream')
 const finished = require('end-of-stream')
 const {
-  VALUE_MAX_SIZE
-} = require('@dswarm/dwebsign')
+  BOX_MAX_SIZE
+} = require('@dwebid/sign')
 
-const PUT_VALUE_MAX_SIZE = VALUE_MAX_SIZE
+const PUT_VALUE_MAX_SIZE = BOX_MAX_SIZE
 
 class UserStore {
   constructor (dht, store) {
@@ -18,148 +18,37 @@ class UserStore {
     this.store = store
     this.prefix = 'u'
   }
-
-  get (key, cb) {
-    assert(Buffer.isBuffer(key), 'Key must be a buffer.')
-    const { store, dht } = this
-    const hasCb = typeof cb === 'function'
-    const storeKey = this.prefix + key.toString('hex')
-    const value = store.get(storeKey)
-    if (value & hasCb) {
-      const { id } = this.dht
-      const localStream = PassThrough({ objectMode: true })
-      finished(localStream, (err) => { 
-        if (err) {
-          cb(err)
-          return
-        }
-        cb(null, value, { id })
-      })
-      localStream.end({id, value})
-      process.nextTick(() => localStream.resume())
-      return localStream
+  get (username, cb) {
+     assert(Buffer.isBuffer(key), 'Username must be a buffer')
+     const { store, dht } = this
+     const hasCb = typeof cb === 'function'
+     const storeKey = this.prefix + username.toString('hex')
+     const value = store.get(storeKey)
+     if (value && hasCb) {
+       const { id } = this.dht
+       const localStream = PassThrough({ objectMode: true })
+       finished(localStream, (err) => {
+         if (err) {
+           cb(err)
+           return
+         }
+         cb(null, value, { id })
+       })
+       localStream.end({ id, value })
+       process.nextTick(() => localStream.resume())
+       return localStream
     }
     let found = false
-    const queryStream = dht.query('user-store', key)
+    const queryStream = dht.query('user-store', username)
       .map((result) => {
         if (!result.value) return
         const { value } = result
         const check = Buffer.alloc(32)
-        const { username, dk } = value
+        const { username, publicKey, dk } = value
         hash(check, username)
-        if (Buffer.compare(check, key) !== 0) return
+        if (Buffer.compare(check, username) !== 0) return
         const { node } = result
         return { id: node.id, value }
-      })
-    if (value && hasCb === false) {
-      process.nextTick(() => {
-        const { id } = this.dht
-        queryStream.emit('data', { id, value })
-      })
-    }
-
-    if (hasCb) {
-      queryStream.once('data', ({id, value}) => {
-        found = true
-        cb(null, value, { id })
-        queryStream.destroy()
-      })
-      finished(queryStream, (err) => {
-        if (err) {
-          cb(err)
-          return
-        }
-        if (found === false) cb(null, null, null)
-      })
-    }
-    return queryStream
-  } // end get
-
-  put (username, opts, cb) {
-    assert(Buffer.isBuffer(username), 'Username must be a Buffer')
-    assert(typeof opts === 'object', 'Options must be an object.')
-    assert(typeof cb === 'function', 'Callback is required.')
-    assert(username.length <= PUT_VALUE_MAX_SIZE, `Username size must be <= ${PUT_VALUE_MAX_SIZE}`)
-    const { store, dht } = this
-    const { dk, publicKey } = opts
-    const key = Buffer.alloc(32)
-    hash(key, username)
-    store.set(this.prefix + key.toString('hex'), username)
-    const queryStream = dht.update('user-store', key, {
-      username, dk, publicKey
-    })
-
-    queryStream.resume()
-    finished(queryStream, (err) => {
-      if (err) {
-        cb(err)
-        return
-      }
-      cb(null, key)
-    })
-    return queryStream
-  } // end put
-
-  _command () {
-    const { store, prefix } = this
-    return {
-      update({ target, value }, cb) {
-        const key = Buffer.alloc(32)
-        const { username, dk, publicKey } = value
-        hash(key, username) 
-        if (Buffer.compare(key, target) !== 0) {
-          cb(Error('ERR_INVALID_INPUT'))
-          return
-        }
-        store.set(prefix + key.toString('hex'), value)
-        cb(null)
-      },
-      query({ target }, cb) {
-        cb(null, store.get(prefix + target.toString('hex')))
-      }
-    
-    }
-  }
-}
-
-class RoomStore {
-  constructor (dht, store) {
-    this.dht = dht
-    this.store = store
-    this.prefix = 'r'
-  }
-
-  get (key, cb) {
-    assert(Buffer.isBuffer(key), 'Key must be a buffer')
-    const { store, dht } = this
-    const hasCb = typeof cb === 'function'
-    const storeKey = this.prefix + key.toString('hex')
-    const value = store.get(storeKey)
-    if (value && hasCb) {
-      const { id } = this.dht
-      const localStream = PassThrough({ objectMode: true })
-      finished(localStream, (err) => {
-        if (err) {
-          cb(err)
-          return
-        }
-        cb(null, value, { id })
-      })
-      localStream.end({ id, value })
-      process.nextTick(() => localStream.resume())
-      return localStream
-    }
-    let found = false
-    const queryStream = dht.query('room-store', key)
-      .map((result) => {
-        if (!result.value) return
-        const { value } = result
-        const check = Buffer.alloc(32)
-        const { roomName, creator, roomKey } = value
-        hash(check, roomName)
-        if (Buffer.compare(check, key) !== 0) return
-        const { node } = result
-        return { id: node.id, value } 
       })
     if (value && hasCb === false) {
       process.nextTick(() => {
@@ -173,7 +62,6 @@ class RoomStore {
         cb(null, value, { id })
         queryStream.destroy()
       })
-
       finished(queryStream, (err) => {
         if (err) {
           cb(err)
@@ -183,18 +71,121 @@ class RoomStore {
       })
     }
     return queryStream
-  } // end get
+  }
+  put (username, opts, cb) {
+    assert(Buffer.isBuffer(username), 'Username must be a buffer.')
+    assert(typeof opts === 'object', 'options must be an object.')
+    assert(typeof cb === 'function', 'callback is required.')
+    assert(username.length <= PUT_VALUE_MAX_SIZE, `Username size must be <= ${PUT_VALUE_MAX_SIZE}`)
+    const { store, dht } = this
+    const { dk, publicKey } = opts
+    const storeValue = { username, dk, publicKey }
+    const key = Buffer.alloc(32)
+    hash(key, username)
+    store.set(this.prefix + key.toString('hex'), storeValue) 
+    const queryStream = dht.update('user-store', key, {
+      username, dk, publicKey
+    })
+    queryStream.resume()
+    finished(queryStream, (err) => {
+      if (err) {
+        cb(err)
+        return
+      }
+      cb(null, key)
+    })
+  }
+  _command () {
+    const { store, prefix } = this
+    return {
+      update({ target, value }, cb) {
+        const key = Buffer.alloc(32)
+        const { username, dk, publicKey } = value
+        hash(key, username)
+        if (Buffer.compare(key, target) !== 0) {
+          cb(Error('ERR_INVALID_INPUT'))
+          return
+        }
+        store.set(prefix + key.toString('hex'), value)
+        cb(null)
+      },
+      query({ target }, cb) {       
+        cb(null, store.get(prefix + target.toString('hex')))
+      }
+    }
+  }
+}
+  class RoomStore {
+    constructor (dht, store) {
+      this.dht = dht
+      this.store = store
+      this.prefix = 'r'
+    }
+    get (room, cb) {
+      assert(Buffer.isBuffer(key), 'Room name must be a buffer.')
+      const { store, dht } = this
+      const hasCb = typeof cb === 'function'
+      const storeKey = this.prefix + room.toString('hex')
+      const value = store.get(storeKey)
+      if (value && hasCb) {
+        const { id } = this.dht
+        const localStream = PassThrough({ objectMode: true })
+        finished(localStream, (err) => {
+          if (err) {
+            cb(err)
+            return
+          }
+          cb(null, value, { id })
+        })
+        localStream.end({ id, value })
+        process.nextTick(() => localStream.resume())
+        return localStream
+      }
+      let found = false
+      const queryStream = dht.query('room-store', room)
+        .map((result) => {
+          if (!result.value) return
+          const { value } = result
+          const check = Buffer.alloc(32)
+          const { roomName, creator, roomKey } = value
+          hash(check, roomName)
+          if (Buffer.compare(check, room !== 0)) return
+          const { node } = result
+          return { id: node.id, value }
+        })
+      if (value && hasCb === false) {
+        process.nextTick(() => {
+          const { id } = this.dht
+          queryStream.emit('data', { id, value })
+        })
+      }
+      if (hasCb) {
+        queryStream.once('data', ({ id, value }) => {
+          found = true
+          cb(null, value, { id })
+          queryStream.destroy()
+        })
+
+        finished(queryStream, (err) => {
+          if (err) {
+            cb(err)
+            return
+          }
+          if (found === false) cb(null, null, null)
+        })
+    }
+    return queryStream
+  }
   put (roomName, opts, cb) {
     assert(Buffer.isBuffer(roomName), 'roomName must be a Buffer.')
-    assert(typeof opts === 'object', 'Opts must be an object.')
-    assert(typeof cb === 'function', 'Callback is required.')
+    assert(typeof opts === 'object', 'opts must be an object')
+    assert(typeof cb === 'function', 'callback is required.')
     assert(roomName.length <= PUT_VALUE_MAX_SIZE, `roomName size must be <= ${PUT_VALUE_MAX_SIZE}`)
-
     const { store, dht } = this
     const { creator, roomKey } = opts
     const key = Buffer.alloc(32)
     hash(key, roomName)
-    store.set(this.prefix + key.toString('hex'), roomName)
+    store.set(this.prefix + key.toString('hex'), storeValue)
     const queryStream = dht.update('room-store', key, {
       roomName, creator, roomKey
     })
@@ -207,12 +198,11 @@ class RoomStore {
       cb(null, key)
     })
     return queryStream
-  } // end put
-
+  }
   _command () {
     const { store, prefix } = this
     return {
-      update ( { target, value }, cb) {``
+      update({ target, value }, cb) {
         const key = Buffer.alloc(32)
         const { roomName, creator, roomKey } = value
         hash(key, username)
@@ -220,14 +210,14 @@ class RoomStore {
           cb(Error('ERR_INVALID_INPUT'))
           return
         }
-        store.set(prefix + key.toString('hex'), value)
+        store.set(prefix + key.toString('hex').value)
         cb(null)
       },
-      query ({ target}, cb) {
+      query ({ target }, cb) {
         cb(null, store.get(prefix + target.toString('hex')))
       }
-    }     
-  } // end _command
+    }
+  }
 }
 
 module.exports = {
